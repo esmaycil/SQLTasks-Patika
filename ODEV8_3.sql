063a202dd1230d1e104d8b560f4d5e80441ba051
Esma Aycil <43879854+esmaycil@users.noreply.github.com>
2021-07-09 20:04:13 +0300

    Update ODEV8_3.sql

--- ODEV8_3.sql
+++ ODEV8_3.sql
@@ -1,9 +1,9 @@
 UPDATE employee
 SET name = 'Esma' WHERE id=5;
 UPDATE employee
-SET bithday = '1998-03-12' WHERE name='Esma';
+SET birthday = '1998-03-12' WHERE name='Esma';
 UPDATE employee
-SET email='update@update.com' WHERE bithday='1998-03-12';
+SET email='update@update.com' WHERE birthday='1998-03-12';
 UPDATE employee
 SET id=105 WHERE email='update@update.com';
 UPDATE employee
