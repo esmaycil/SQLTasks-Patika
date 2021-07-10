562d5eb1a59d225c11272b0e4e35d44d8042274a
Esma Aycil <43879854+esmaycil@users.noreply.github.com>
2021-07-10 04:34:04 +0300

    Create Odev11.sql

---
+++ Odev11.sql
@@ -0,0 +1,32 @@
+-- 1.actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+
+(SELECT first_name FROM actor)
+UNION
+(SELECT first_name FROM customer);
+
+-- 2.actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+(SELECT first_name FROM actor)
+INTERSECT
+(SELECT first_name FROM customer);
+
+-- 3. actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+
+(SELECT first_name FROM actor)
+except
+(SELECT first_name FROM customer);
+
+--4. ilk üç sorguyu tekrar eden veriler için de yapalım.
+-- actor ve customer tablolarındaki last_namelerin karşılaştırılması
+
+(SELECT last_name FROM actor)
+UNION
+(SELECT last_name FROM customer);
+
+(SELECT last_name FROM actor)
+INTERSECT
+(SELECT last_name FROM customer);
+
+(SELECT last_name FROM actor)
+EXCEPT
+(SELECT last_name FROM customer);
